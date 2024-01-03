a2575edf009fd8aeb691c88d1e6c4777acadd5d4
amandakarolline <amandakarolline@hotmail.com>
2024-01-03 00:54:02 -0300

    Alterando settings

--- webdev/settings.py
+++ webdev/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s$)9x!)=_za--37vnq2#6h#)m9qp3-)1lfn!_rgap26rx4whtk'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
